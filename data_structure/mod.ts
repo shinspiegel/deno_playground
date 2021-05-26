188c9ed1be53e95a6fcbb0a1f243167b6799b197
Jeferson 'Shin <shin.jef@gmail.com>
2021-05-25 21:48:13 -0300

    Added new structure for linked list

--- data_structure/mod.ts
+++ data_structure/mod.ts
@@ -3,17 +3,4 @@
 import Stack from "./Stack.ts";
 import Queue from "./Queue.ts";
 import LinkedList from "./LinkedList.ts";
-
-const t = new Stack<number>();
-
-t.insert(1);
-t.insert(2);
-t.insert(3);
-
-console.log(t);
-
-console.log(t.peek());
-console.log(t.remove());
-console.log(t.peek());
-
-console.log(t);
+import DoubleLinkedList from "./DoubleLinkedList.ts";
